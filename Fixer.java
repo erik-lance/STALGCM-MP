1f1894b01892f57120aa5d0c8eee28433589985a
erik-lance <eriklance@gmail.com>
2022-06-16 00:55:00 +0800

    Basic Attribs

--- Fixer.java
+++ Fixer.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+
 public class Fixer {
-    
+    private String name;
+    private ArrayList<State> states;
+
+    public void convertToDFA(Machine m) {
+        
+    }
 }
